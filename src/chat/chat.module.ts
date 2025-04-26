1c04bc109ce7b0a19f9d71ab8b11808ab9875dc7
jiminKang <wlals1234560@gmail.com>
2025-04-26 15:07:40 +0900

    인스턴스가 2개 생성되는 예시 재현

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 
 @Module({
-//   providers: [ChatGateway],
-//   exports:[ChatGateway]
+  providers: [ChatGateway],
+  exports:[ChatGateway]
 })
 export class ChatModule {}
